e948d71303336fa1101d39839aa0df403cbab3ad
Matthieu Barraque <matthieu.barraque@outlook.fr>
2025-02-17 11:46:38 +0100

    [TEST] -> github Actions V3

--- database/init-mongo.js
+++ database/init-mongo.js
@@ -1,32 +1,16 @@
 db = db.getSiblingDB('catalog');
 
+// Créer la collection "products" vide
 db.createCollection('products');
 
-db.products.insertMany([
-  {
-    name: "Produit 1",
-    description: "Description du produit 1",
-    price: 19.99
-  },
-  {
-    name: "Produit 2",
-    description: "Description du produit 2",
-    price: 29.99
-  },
-  {
-    name: "Produit 3",
-    description: "Description du produit 3",
-    price: 39.99
-  }
-]);
-
+// Créer la collection "users" et insérer des documents d'exemple
 db.createCollection('users');
 
 db.users.insertMany([
   {
     username: "admin",
     email: "admin@example.com",
-    password: "$2a$10$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", // Mot de passe hashé
+    password: "$2a$10$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     firstName: "Admin",
     lastName: "User",
     roles: ["ADMIN", "USER"],
@@ -37,7 +21,7 @@ db.users.insertMany([
   {
     username: "user1",
     email: "user1@example.com",
-    password: "$2a$10$yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy", // Mot de passe hashé
+    password: "$2a$10$yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
     firstName: "User",
     lastName: "One",
     roles: ["USER"],
@@ -48,7 +32,7 @@ db.users.insertMany([
   {
     username: "user2",
     email: "user2@example.com",
-    password: "$2a$10$zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz", // Mot de passe hashé
+    password: "$2a$10$zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
     firstName: "User",
     lastName: "Two",
     roles: ["USER"],
